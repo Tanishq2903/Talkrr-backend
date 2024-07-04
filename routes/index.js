02d3b076563a239944511ca623c8f5a47a1a9c64
Tanishq2903 <maharshitanishq@gmail.com>
2024-07-04 20:05:05 +0530

    add sockets

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const loginUser = require('../controllers/loginUser');
 const checkEmail = require('../controllers/checkEmail');
 const userDetails = require('../controllers/userDetails');
 const updateUserDetails = require('../controllers/updateUserDetails');
+const logout = require('../controllers/logout');
 
 const router = express.Router()
 
@@ -13,5 +14,7 @@ router.post('/email',checkEmail)
 router.post('/login',loginUser)
 router.get('/user',userDetails)
 router.post('/update',updateUserDetails)
+router.get('/logout',logout)
+
 
 module.exports = router
