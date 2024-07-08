74df4f3779c854510a6e6d65f2f8b989d2c20391
Tanishq2903 <maharshitanishq@gmail.com>
2024-07-08 20:45:16 +0530

    sockets bug fixed

--- helper/getConversation.js
+++ helper/getConversation.js
@@ -1,39 +1,48 @@
-const { ConversationModel } = require("../models/ConversationModel.js")
+const { ConversationModel } = require("../models/ConversationModel.js");
 
-const getConversation = async(currentUserId)=>{
-    if(currentUserId){
+const getConversation = async (currentUserId) => {
+    if (!currentUserId) return [];
+
+    try {
+        // Fetch conversations where currentUserId is either sender or receiver
         const currentUserConversation = await ConversationModel.find({
-            "$or" : [
-                { sender : currentUserId },
-                { receiver : currentUserId}
+            "$or": [
+                { sender: currentUserId },
+                { receiver: currentUserId }
             ]
-        }).sort({  updatedAt : -1 }).populate('messages').populate('sender').populate('receiver')
-
-        const conversation = currentUserConversation.map((conv)=>{
-            const countUnseenMsg = conv?.messages?.reduce((preve,curr) => {
-                const msgByUserId = curr?.msgByUserId?.toString()
-
-                if(msgByUserId !== currentUserId){
-                    return  preve + (curr?.seen ? 0 : 1)
-                }else{
-                    return preve
-                }
-             
-            },0)
-            
-            return{
-                _id : conv?._id,
-                sender : conv?.sender,
-                receiver : conv?.receiver,
-                unseenMsg : countUnseenMsg,
-                lastMsg : conv.messages[conv?.messages?.length - 1]
-            }
         })
+            .sort({ updatedAt: -1 })
+            .populate('messages')
+            .populate('sender') // Assuming these fields are ObjectIds that reference UserModel
+            .populate('receiver');
 
-        return conversation
-    }else{
-        return []
+        // Process conversations
+        const conversation = currentUserConversation.map((conv) => {
+            // Handle cases where messages might be empty
+            const lastMsg = conv.messages.length > 0 ? conv.messages[conv.messages.length - 1] : null;
+
+            // Count unseen messages
+            const countUnseenMsg = conv.messages.reduce((prev, curr) => {
+                if (curr.msgByUserId.toString() !== currentUserId) {
+                    return prev + (curr.seen ? 0 : 1);
+                }
+                return prev;
+            }, 0);
+
+            return {
+                _id: conv._id,
+                sender: conv.sender,
+                receiver: conv.receiver,
+                unseenMsg: countUnseenMsg,
+                lastMsg: lastMsg
+            };
+        });
+
+        return conversation;
+    } catch (error) {
+        console.error('Error fetching conversations:', error);
+        return [];
     }
-}
+};
 
-module.exports = getConversation
+module.exports = getConversation;
