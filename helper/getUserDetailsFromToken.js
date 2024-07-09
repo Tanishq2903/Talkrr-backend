f610c4b1fc00550e2ee762fa13850fdf046a06e7
Tanishq2903 <maharshitanishq@gmail.com>
2024-07-09 20:28:29 +0530

    message issue resolved

--- helper/getUserDetailsFromToken.js
+++ helper/getUserDetailsFromToken.js
@@ -1,20 +1,35 @@
-const jwt = require('jsonwebtoken')
-const UserModel = require('../models/UserModel.js')
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/UserModel.js');
 
-const getUserDetailsFromToken = async(token)=>{
-    
-    if(!token){
+const getUserDetailsFromToken = async (token) => {
+    if (!token) {
+        console.log('No token provided');
         return {
-            message : "session out",
-            logout : true,
-        }
+            message: "session out",
+            logout: true,
+        };
     }
 
-    const decode = await jwt.verify(token,process.env.JWT_SECRET_KEY)
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await UserModel.findById(decode.id).select('-password');
 
-    const user = await UserModel.findById(decode.id).select('-password')
+        if (!user) {
+            console.log('User not found for token:', token);
+            return {
+                message: "User not found",
+                logout: true,
+            };
+        }
 
-    return user
-}
+        return user;
+    } catch (error) {
+        console.error('Error verifying token or fetching user:', error);
+        return {
+            message: "session out",
+            logout: true,
+        };
+    }
+};
 
-module.exports = getUserDetailsFromToken
+module.exports = getUserDetailsFromToken;
